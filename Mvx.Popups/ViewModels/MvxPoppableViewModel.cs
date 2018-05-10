97268073fb06d86e417fe82ea61f86842a222005
Daniel Bautista Hernández <Danielbahe@gmail.com>
2018-05-10 14:43:25 +0200

    gitflow-feature-stash: mvx_returnable_vm
    Added return type to Mvx vm and naming the generic types

--- Mvx.Popups/ViewModels/MvxPoppableViewModel.cs
+++ Mvx.Popups/ViewModels/MvxPoppableViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MvvmCross.Core.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Popups.Interfaces;
@@ -9,29 +8,34 @@ namespace Xamarin.Popups.ViewModels
     public abstract class MvxPoppableViewModel : MvxViewModel, IPoppableViewModel
     {
         private IPoppable _view;
+
         public void SetViewBinding(object view)
         {
-            if(_view == null) _view = view as IPoppable;
+            if (_view == null) _view = view as IPoppable;
         }
 
         public async Task<bool> ShowAlert(string title, string message, string okText = "Ok", string cancelText = null)
         {
             return await _view.ShowAlert(title, message, okText, cancelText);
         }
+
         public void ShowLoading(string loadingImage = null, string loadingText = null)
         {
             _view.ShowLoading(loadingImage, loadingText);
         }
+
         public void HideLoading()
         {
             _view.HideLoading();
         }
+
         public async Task<T> ShowFormCustomPopUp<T>(IWidget<T> view, bool animate = false)
         {
             var result = await _view.ShowFormCustomPopUp<T>(view, animate);
             _view.HidePopUps();
             return result;
         }
+
         public async Task ShowCustomPopUp(ContentView view)
         {
             await _view.ShowCustomPopUp(view);
@@ -43,9 +47,10 @@ namespace Xamarin.Popups.ViewModels
         }
     }
 
-    public abstract class MvxPoppableViewModel<T> : MvxViewModel<T>, IPoppableViewModel
+    public abstract class MvxPoppableViewModel<TParameter> : MvxViewModel<TParameter>, IPoppableViewModel
     {
         private IPoppable _view;
+
         public void SetViewBinding(object view)
         {
             if (_view == null) _view = view as IPoppable;
@@ -55,20 +60,67 @@ namespace Xamarin.Popups.ViewModels
         {
             return await _view.ShowAlert(title, message, okText, cancelText);
         }
+
         public void ShowLoading(string loadingImage = null, string loadingText = null)
         {
             _view.ShowLoading(loadingImage, loadingText);
         }
+
         public void HideLoading()
         {
             _view.HideLoading();
         }
+
         public async Task<T> ShowFormCustomPopUp<T>(IWidget<T> view, bool animate = false)
         {
             var result = await _view.ShowFormCustomPopUp<T>(view, animate);
             _view.HidePopUps();
             return result;
         }
+
+        public async Task ShowCustomPopUp(ContentView view)
+        {
+            await _view.ShowCustomPopUp(view);
+        }
+
+        public void HidePopUps()
+        {
+            _view.HidePopUps();
+        }
+    }
+
+    public abstract class MvxPoppableViewModel<TParameter, TResult> : MvxViewModel<TParameter, TResult>,
+        IPoppableViewModel
+    {
+        private IPoppable _view;
+
+        public void SetViewBinding(object view)
+        {
+            if (_view == null) _view = view as IPoppable;
+        }
+
+        public async Task<bool> ShowAlert(string title, string message, string okText = "Ok", string cancelText = null)
+        {
+            return await _view.ShowAlert(title, message, okText, cancelText);
+        }
+
+        public void ShowLoading(string loadingImage = null, string loadingText = null)
+        {
+            _view.ShowLoading(loadingImage, loadingText);
+        }
+
+        public void HideLoading()
+        {
+            _view.HideLoading();
+        }
+
+        public async Task<T> ShowFormCustomPopUp<T>(IWidget<T> view, bool animate = false)
+        {
+            var result = await _view.ShowFormCustomPopUp<T>(view, animate);
+            _view.HidePopUps();
+            return result;
+        }
+
         public async Task ShowCustomPopUp(ContentView view)
         {
             await _view.ShowCustomPopUp(view);
