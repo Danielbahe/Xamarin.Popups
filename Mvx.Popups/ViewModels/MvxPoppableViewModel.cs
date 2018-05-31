44cf1ef61eeb9cd95f12acd43d0da580d9b4de90
Daniel Bautista Hernández <daniel.bautista@sf.zetes.com>
2018-05-31 12:25:53 +0200

    Refactor to be able to mock nuget

--- Mvx.Popups/ViewModels/MvxPoppableViewModel.cs
+++ Mvx.Popups/ViewModels/MvxPoppableViewModel.cs
@@ -1,134 +1,36 @@
-﻿using System.Threading.Tasks;
-using MvvmCross.Core.ViewModels;
-using Xamarin.Forms;
+﻿using MvvmCross.Core.ViewModels;
 using Xamarin.Popups.Interfaces;
 
 namespace Xamarin.Popups.ViewModels
 {
     public abstract class MvxPoppableViewModel : MvxViewModel, IPoppableViewModel
     {
-        private IPoppable _view;
+        public IPoppable PoppableService { get; set; }
 
         public void SetViewBinding(object view)
         {
-            if (_view == null) _view = view as IPoppable;
-        }
-
-        public async Task<bool> ShowAlert(string title, string message, string okText = "Ok", string cancelText = null)
-        {
-            return await _view.ShowAlert(title, message, okText, cancelText);
-        }
-
-        public void ShowLoading(string loadingImage = null, string loadingText = null)
-        {
-            _view.ShowLoading(loadingImage, loadingText);
-        }
-
-        public void HideLoading()
-        {
-            _view.HideLoading();
-        }
-
-        public async Task<T> ShowFormCustomPopUp<T>(IWidget<T> view, bool animate = false)
-        {
-            var result = await _view.ShowFormCustomPopUp<T>(view, animate);
-            _view.HidePopUps();
-            return result;
-        }
-
-        public async Task ShowCustomPopUp(ContentView view)
-        {
-            await _view.ShowCustomPopUp(view);
-        }
-
-        public void HidePopUps()
-        {
-            _view.HidePopUps();
+            if (PoppableService == null) PoppableService = view as IPoppable;
         }
     }
 
     public abstract class MvxPoppableViewModel<TParameter> : MvxViewModel<TParameter>, IPoppableViewModel
     {
-        private IPoppable _view;
+        public IPoppable PoppableService { get; set; }
 
         public void SetViewBinding(object view)
         {
-            if (_view == null) _view = view as IPoppable;
-        }
-
-        public async Task<bool> ShowAlert(string title, string message, string okText = "Ok", string cancelText = null)
-        {
-            return await _view.ShowAlert(title, message, okText, cancelText);
-        }
-
-        public void ShowLoading(string loadingImage = null, string loadingText = null)
-        {
-            _view.ShowLoading(loadingImage, loadingText);
-        }
-
-        public void HideLoading()
-        {
-            _view.HideLoading();
-        }
-
-        public async Task<T> ShowFormCustomPopUp<T>(IWidget<T> view, bool animate = false)
-        {
-            var result = await _view.ShowFormCustomPopUp<T>(view, animate);
-            _view.HidePopUps();
-            return result;
-        }
-
-        public async Task ShowCustomPopUp(ContentView view)
-        {
-            await _view.ShowCustomPopUp(view);
-        }
-
-        public void HidePopUps()
-        {
-            _view.HidePopUps();
+            if (PoppableService == null) PoppableService = view as IPoppable;
         }
     }
 
     public abstract class MvxPoppableViewModel<TParameter, TResult> : MvxViewModel<TParameter, TResult>,
         IPoppableViewModel
     {
-        private IPoppable _view;
+        public IPoppable PoppableService { get; set; }
 
         public void SetViewBinding(object view)
         {
-            if (_view == null) _view = view as IPoppable;
-        }
-
-        public async Task<bool> ShowAlert(string title, string message, string okText = "Ok", string cancelText = null)
-        {
-            return await _view.ShowAlert(title, message, okText, cancelText);
-        }
-
-        public void ShowLoading(string loadingImage = null, string loadingText = null)
-        {
-            _view.ShowLoading(loadingImage, loadingText);
-        }
-
-        public void HideLoading()
-        {
-            _view.HideLoading();
-        }
-
-        public async Task<T> ShowFormCustomPopUp<T>(IWidget<T> view, bool animate = false)
-        {
-            var result = await _view.ShowFormCustomPopUp<T>(view, animate);
-            _view.HidePopUps();
-            return result;
-        }
-
-        public async Task ShowCustomPopUp(ContentView view)
-        {
-            await _view.ShowCustomPopUp(view);
-        }
-
-        public void HidePopUps()
-        {
-            _view.HidePopUps();
+            if (PoppableService == null) PoppableService = view as IPoppable;
         }
     }
 }
